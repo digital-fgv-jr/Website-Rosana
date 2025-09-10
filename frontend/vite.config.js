826cc6cffaf9089d4fd667cf5170a8080f755ea2
brunolzrosa <brunoluiszrosa@gmail.com>
2025-09-10 02:12:35 +0000

    docker working

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,15 +5,11 @@ import tailwindcss from 'tailwindcss'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // Permite que o servidor seja acessado pela rede do Docker
     host: true, 
-    // Porta padrão do Vite
     port: 5173, 
-    // Configuração do proxy
     proxy: {
-      // Qualquer requisição para /api será redirecionada para o backend
       '/api': {
-        target: 'http://backend:8000', // 'backend' é o nome do serviço no docker-compose
+        target: 'http://backend:8000',
         changeOrigin: true,
         secure: false,
       },
