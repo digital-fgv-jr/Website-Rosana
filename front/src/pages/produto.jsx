ca93a8b4f5d0e70a09d573dd9b6b57b1508ad83d
João Gabriel Calbo <profjoaogabriel.mat@gmail.com>
2025-09-09 23:14:46 -0300

    v5

--- front/src/pages/produto.jsx
+++ front/src/pages/produto.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { CirclePlus, CircleMinus } from "lucide-react";
 import MiniCarrinho from "../components/MiniCarrinho";
 import { produtos } from "../data/produtos";
 import ProdutosRelacionados from "../components/ProdutosRelacionados";
@@ -89,21 +90,10 @@ export default function Produto() {
 
       <main className="flex-grow bg-brancoperola py-12 px-6">
         <div className="flex justify-center w-full">
-          <div className="flex flex-col lg:flex-row max-w-6xl w-full px-4 gap-16">
-            {/* Lado esquerdo: imagem + breadcrumb */}
-            <div className="flex flex-col items-start w-full lg:w-auto max-w-md">
-              <p className="text-gray-500 text-sm mb-2">
-                Início / Joias / {produto.categoria.nome_categoria} / {produto.nome}
-              </p>
-              <img
-                src={produto.imagens[0].imagem}
-                alt={produto.nome}
-                className="w-full h-auto object-cover "
-              />
-            </div>
-
-            {/* Lado direito: detalhes */}
-            <div className="flex flex-col justify-start lg:justify-between w-full lg:w-1/2 lg:pl-12">
+          <div className="flex flex-col-reverse lg:flex-row max-w-6xl w-full px-4 gap-16">
+            
+            {/* Lado esquerdo (desktop) / abaixo da imagem (mobile): detalhes */}
+            <div className="flex flex-col justify-start lg:justify-between w-full lg:w-1/2 lg:pr-12">
               <div>
                 <h1 className="text-3xl font-RoxboroughCFRegular text-gray-800 mt-5 ml-auto mb-4">
                   {produto.nome}
@@ -117,47 +107,62 @@ export default function Produto() {
               <p className="text-gray-700 mt-8">{produto.descricao}</p>
               
               {/* Contador de quantidade */}
-              <div className="flex items-center gap-2 mt-6 mb-3">
-                <button
-                  onClick={diminuir}
-                  className="px-3 py-1 bg-gray-200 hover:bg-gray-300 transition"
-                >
-                  -
+              <div className="flex items-center gap-2 mt-6 mb-5">
+                <button onClick={diminuir}>
+                  <CircleMinus
+                    className="w-6 h-6 text-gray-600 transition-transform duration-200 hover:scale-110 hover:[#c2b280] cursor-pointer"
+                  />
                 </button>
-                <span className="px-3 py-1 border text-gray-800">{quantidade}</span>
-                <button
-                  onClick={aumentar}
-                  className="px-3 py-1 bg-gray-200 hover:bg-gray-300 transition"
-                >
-                  +
+                <span className="w-6 text-center text-gray-700 font-medium">{quantidade}</span>
+                <button onClick={aumentar}>
+                  <CirclePlus
+                    className="w-6 h-6 text-gray-600 transition-transform duration-200 hover:scale-110 hover:text-[#c2b280] cursor-pointer"
+                  />
                 </button>
               </div>
 
               {/* Quantidade disponível */}
               <p className="text-sm text-gray-600 mb-4">
-                Quantidade disponível em estoque: {produto.qtd_disponivel}
+                Quantidade em estoque: {produto.qtd_disponivel}
               </p>
 
               {/* Botão adicionar ao carrinho */}
               <button
                 onClick={adicionarAoCarrinho}
-                className="mt-auto px-6 py-3 bg-[#1c2c3c] max-w-[400px] font-MontserratRegular text-brancoperola hover:bg-[#25384d] transition"
+                className="
+                  mt-auto px-6 py-3 bg-[#1c2c3c] max-w-[200px] font-MontserratRegular
+                  text-brancoperola rounded-lg shadow-sm hover:bg-[#25384d] hover:shadow-md
+                  transform transition duration-300 ease-in-out"
               >
                 Adicionar à sacola
               </button>
             </div>
+
+            {/* Lado direito (desktop) / acima dos detalhes (mobile): imagem + breadcrumb */}
+            <div className="flex flex-col items-start w-full lg:w-auto max-w-md lg:ml-auto">
+              <p className="text-gray-500 text-sm mb-2">
+                Início / Joias / {produto.categoria.nome_categoria} / {produto.nome}
+              </p>
+              <div className="w-full border border-[#c0c0c0] shadow-sm bg-white">
+                <img
+                  src={produto.imagens[0].imagem}
+                  alt={produto.nome}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            </div>
+
           </div>
         </div>
       </main>
 
       {/* Seção de descrição e detalhes */}
-        <div className="mt-12 max-w-6xl mx-auto px-4">
+       <div className="mt-12 max-w-6xl w-[60%] px-4 lg:pl-12">
           {/* Título */}
           <h2 className="text-2xl font-RoxboroughCFRegular text-gray-800 mb-3">
             Detalhes
           </h2>
           <hr className="border-t-2 border-gray-300 mb-6 w-full" />
-
           {/* Detalhes gerais */}
           <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mb-5">
             {produto.detalhes.map((detalhe, idx) => (
